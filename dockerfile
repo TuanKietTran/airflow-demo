58a7f8024d1fb56cfc8b9dc6add579b6525375be
TuanKietTran <kyletran101.work@gmail.com>
2024-08-16 21:14:51 +0700

    add example and unfinished dag

--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
 FROM apache/airflow:2.9.3
 COPY requirements.txt /
-RUN pip install --no-cache-dir "apache-airflow==${AIRFLOW_VERSION}" -r /requirements.txt
+RUN pip install --no-cache-dir -r /requirements.txt
 USER root
 
 USER airflow
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# If in remote mode, clone the DAGs repository
-if [ "$DAG_SOURCE" = "remote" ]; then
-    git clone $GIT_REPO_URL /usr/local/airflow/dags
-fi
-
-# Start the Airflow service (webserver or scheduler)
-exec airflow "$@"
